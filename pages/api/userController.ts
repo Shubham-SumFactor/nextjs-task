2a0abc19f56f40e2c09df79c75fc8e5c7155e085
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-30 16:55:20 +0530

    CRUD Operations implemented

--- pages/api/userController.ts
+++ pages/api/userController.ts
@@ -2,20 +2,28 @@ import { executeQuery } from '../../lib/connectDatabase'
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function userController(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
+    if (req.method === 'POST') {
     return new Promise(async (resolve, reject) => {
         try {
-             const sqlQuery = `insert into users_table (f_name, l_name, email, password) values ("Shubham", "Thakur", "shubhamthakur@gmail.com", "shubham1234")`;
+
+            let { f_name, l_name, email, password } = req.body
+            const sqlQuery = `insert into users_table (f_name, l_name, email, password) values ('${f_name}','${l_name}' ,'${email}', '${password}')`;
+
+            // const sqlQuery = `insert into users_table (f_name, l_name, email, password) values ("Shubham", "Singh", "shubham6767@gmail.com", "shubham123456")`;
             //  const sqlQuery = `select * from users_table`;
+            // const sqlQuery = `update users_table set f_name='Shubhi' where email="shubham67@gmail.com"`
+            //  const sqlQuery = `delete from users_table where email="shubham6767@gmail.com"`
             let response = await executeQuery(sqlQuery)
  
             console.log("return newPromise response:", response)
 
-            return resolve(response) } 
+            // return resolve(response)
+            res.status(200).json(response); } 
             
             catch (error) {
                 console.log("return newPromise error:", error)
-                return reject(error)
+                // return reject(error)
+                res.status(500).json({ error: 'Internal Server Error' });
             }
         })
     }}
