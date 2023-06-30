1e3f040e959328b51bc345dbd79f137eb69c3a1c
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-30 17:20:13 +0530

    User Duplicacy validation for registration

--- pages/api/userController.ts
+++ pages/api/userController.ts
@@ -7,14 +7,23 @@ export default async function userController(req: NextApiRequest, res: NextApiRe
         try {
 
             let { f_name, l_name, email, password } = req.body
-            const sqlQuery = `insert into users_table (f_name, l_name, email, password) values ('${f_name}','${l_name}' ,'${email}', '${password}')`;
+            // const sqlQuery = `insert into users_table (f_name, l_name, email, password) values ('${f_name}','${l_name}' ,'${email}', '${password}')`;
 
             // const sqlQuery = `insert into users_table (f_name, l_name, email, password) values ("Shubham", "Singh", "shubham6767@gmail.com", "shubham123456")`;
             //  const sqlQuery = `select * from users_table`;
             // const sqlQuery = `update users_table set f_name='Shubhi' where email="shubham67@gmail.com"`
             //  const sqlQuery = `delete from users_table where email="shubham6767@gmail.com"`
-            let response = await executeQuery(sqlQuery)
+            // let response = await executeQuery(sqlQuery)
  
+            //=========================== AVOID DUPLICACY ===========================
+            const getRecord = `select * from users_table where email='${email}'`;
+            let resultset: any = await executeQuery(getRecord);
+            if (resultset.length > 0) return res.status(400).send({ message: "User already registered please login" });
+            //=======================================================================
+            
+           
+            const sqlQuery = `insert into users_table (f_name, l_name, email, password) values ('${f_name}','${l_name}' ,'${email}', '${password}')`;
+            let response = await executeQuery(sqlQuery);
             console.log("return newPromise response:", response)
 
             // return resolve(response)
