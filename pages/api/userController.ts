38e7646336291fa5340c7a5b97d58ec4c6e04395
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-07-01 16:05:56 +0530

    password encrypted using bcrypt

--- pages/api/userController.ts
+++ pages/api/userController.ts
@@ -1,5 +1,6 @@
 import { executeQuery } from '../../lib/connectDatabase'
 import { NextApiRequest, NextApiResponse } from 'next';
+import bcrypt from 'bcrypt';
 
 export default async function userController(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
@@ -21,8 +22,12 @@ export default async function userController(req: NextApiRequest, res: NextApiRe
             if (resultset.length > 0) return res.status(400).send({ message: "User already registered please login" });
             //=======================================================================
             
+            // Encrypting the Password========================
+            const  salt = await bcrypt.genSalt();
+            const hashedPassword = await bcrypt.hash(password,salt);
+
            
-            const sqlQuery = `insert into users_table (f_name, l_name, email, password) values ('${f_name}','${l_name}' ,'${email}', '${password}')`;
+            const sqlQuery = `insert into users_table (f_name, l_name, email, password) values ('${f_name}','${l_name}' ,'${email}', '${hashedPassword}')`;
             let response = await executeQuery(sqlQuery);
             console.log("return newPromise response:", response)
 
@@ -35,4 +40,6 @@ export default async function userController(req: NextApiRequest, res: NextApiRe
                 res.status(500).json({ error: 'Internal Server Error' });
             }
         })
+    }else{
+        res.send({done:false});
     }}
