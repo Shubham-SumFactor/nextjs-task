92d6034df4707ce2ae5b39d3267ef5a5e617b05d
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-07-03 16:46:54 +0530

    login implemented and tokens generated

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,6 +1,7 @@
 import { executeQuery } from "@/lib/connectDatabase";
 import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
+import { generateAccessToken, refreshAccessToken } from "@/lib/tokens";
 
 export default async function login(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
@@ -9,16 +10,18 @@ export default async function login(req: NextApiRequest, res: NextApiResponse) {
       const getRecord = `SELECT * FROM users_table WHERE email = '${email}'`;
       const resultset: any = await executeQuery(getRecord);
 
-      if (resultset.length === 0) {
-        res.status(400).send({ message: "User doesn't exist, Register first" });
-      } else {
+      if (resultset.length === 0) res.status(400).send({ message: "User doesn't exist, Register first" });
+      
         const match: boolean = await bcrypt.compare(password, resultset[0].password);
-        if (!match) {
-          res.status(400).send({ message: "Incorrect password" });
-        } else {
-          res.status(200).send({ message: "Login successful" });
-        }
-      }
+        if (!match) return res.status(400).send({ message: "Incorrect password" });
+  
+        let user: any = { email: email as string, password: password as string };
+
+        let JwtToken = generateAccessToken(user);
+
+        const JwtRefreshtoken = refreshAccessToken({ user });
+
+        res.status(200).send({ JwtToken, JwtRefreshtoken, message: "Login successful"});
     } catch (error) {
       res.status(400).send({ done: false });
     }
