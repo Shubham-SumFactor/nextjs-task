985ede465696d59bdb6b05e37ad207eb1cc66b2d
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-07-05 18:19:11 +0530

    refresh token implemented

---
+++ pages/api/refreshToken.ts
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+import { generateAccessToken } from '@/lib/tokens';
+import { executeQuery } from '@/lib/connectDatabase';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function refreshToken(req: NextApiRequest, res: NextApiResponse) {
+  
+  try{
+  const { token, email } = req.body;
+
+  if (!token)
+    return res.status(401).json({ error: 'Token is required' });
+  
+
+  const getRecord = `select * from users_table where email='${email}';`;
+
+  const result: any = await executeQuery(getRecord);
+
+  if (result.length === 0) 
+    return res.status(403).json({ error: 'User not found' });
+  
+
+  jwt.verify(token, process.env.REFRESH_ACCESS_KEY as string, (error: unknown, response: unknown) => {
+    if (error) return res.status(403).json({ error: 'Forbidden' });
+
+    const accessToken: string = generateAccessToken({
+      email: result[0].email,
+      password: result[0].password,
+    });
+
+    return res.status(200).json({ token: `Bearer ${accessToken}` });
+  });
+   
+// 
+    // return res.json({ token:` Bearer{accessToken}` });
+}catch (error) {
+  return res.status(403).json({ error: error });
+}
+}
